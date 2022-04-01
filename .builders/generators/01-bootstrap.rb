5a18ead1c3b26a400ee92551cfc8b4912b394a96
David Cruwys <david@ideasmen.com.au>
2022-04-02 10:19:13 +1100

    chore: github actions (ci/cd)

--- .builders/generators/01-bootstrap.rb
+++ .builders/generators/01-bootstrap.rb
@@ -68,7 +68,7 @@ KManager.action :bootstrap do
         run_command("git add .; git commit -m 'chore: #{self.options.description.downcase}'; git push")
       end
       .blueprint(
-        active: true,
+        active: false,
         name: :opinionated,
         description: 'opinionated GEM files',
         on_exist: :write) do
@@ -99,7 +99,7 @@ KManager.action :bootstrap do
         run_command("git add .; git commit -m 'chore: #{self.options.description.downcase}'; git push")
       end
       .blueprint(
-        active: false,
+        active: true,
         name: :ci_cd,
         description: 'github actions (CI/CD)',
         on_exist: :write) do
