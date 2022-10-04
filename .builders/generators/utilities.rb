c4ab684ed18fd46bd838a70745621393de90da00
David Cruwys <david@ideasmen.com.au>
2022-10-04 16:21:23 +1100

    update raw component graph

--- .builders/generators/utilities.rb
+++ .builders/generators/utilities.rb
@@ -1,7 +1,7 @@
 KManager.action :utilities do
   helpers = self
   action do
-    director = KDirector::Dsls::BasicDsl
+    KDirector::Dsls::BasicDsl
       .init(k_builder,
         template_folder: '',
         on_exist:                   :write,                      # %i[skip write compare]
@@ -10,23 +10,26 @@ KManager.action :utilities do
       .blueprint(name: :build_design_system) do
         cd(:data)
 
-        helpers.build_design_system_graph(self)
+        json = helpers.build_design_system_graph# (self)
+
+        add('design_system.json', content: json)
       end
   end
 
-  def build_design_system_graph(director)
+  def build_design_system_graph
     source_path = File.expand_path('~/dev/kgems/k_templates/templates/tailwind')
-    target_path = File.join(director.k_builder.target_folders.get(:data), 'design_system.json')
 
     graph = TailwindDsl::RawComponents::ComponentGraph.new
 
-    graph.add_design_system(source_path, name: :tui)
-    graph.add_design_system(source_path, name: :codepen)
-    graph.add_design_system(source_path, name: :devdojo)
-    graph.add_design_system(source_path, name: :merakiui)
-    graph.add_design_system(source_path, name: :noq)
-    graph.add_design_system(source_path, name: "starter-kit")
-    graph.write(target_path)
+    graph.add_design_system(File.join(source_path, 'tui'))
+    graph.add_design_system(File.join(source_path, 'codepen'))
+    graph.add_design_system(File.join(source_path, 'devdojo'))
+    graph.add_design_system(File.join(source_path, 'merakiui'))
+    graph.add_design_system(File.join(source_path, 'noq'))
+    graph.add_design_system(File.join(source_path, 'starter-kit'))
+
+    JSON.pretty_generate(graph.to_h)
+    
   end
 end
 KManager.opts.sleep                       = 2
