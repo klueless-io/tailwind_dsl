30edfa985b3ec7c06d6997ee53f1b009f64fe803
David Cruwys <david@ideasmen.com.au>
2022-10-24 14:41:27 +1100

    fix: before GHA flag

--- spec/tailwind_dsl/etl/sample_spec.rb
+++ spec/tailwind_dsl/etl/sample_spec.rb
@@ -2,49 +2,49 @@
 
 require 'spec_helper'
 
-# RSpec.describe TailwindDsl::Etl::RawComponents::Load do
-#   let(:instance) { described_class.new }
-#   let(:target_root_path) { File.join(SPEC_FOLDER, 'samples/00-data') }
+RSpec.describe TailwindDsl::Etl::RawComponents::Load do
+  let(:instance) { described_class.new }
+  let(:target_root_path) { File.join(SPEC_FOLDER, 'samples/00-data') }
 
-#   context 'sample' do
-#     let(:source_root_path) { 'spec/samples/01-raw_components' }
+  context 'sample' do
+    let(:source_root_path) { 'spec/samples/01-raw_components' }
 
-#     before do
-#       instance.add_design_system(File.join(source_root_path, 'tui'))
-#       instance.add_design_system(File.join(source_root_path, 'noq'))
-#     end
+    before do
+      instance.add_design_system(File.join(source_root_path, 'tui'))
+      instance.add_design_system(File.join(source_root_path, 'noq'))
+    end
 
-#     it { instance.write(File.join(target_root_path, 'uikit.test.json')) }
-#   end
+    it { instance.write(File.join(target_root_path, 'uikit.test.json')) }
+  end
 
-#   context 'live' do
-#     let(:source_root_path) { File.expand_path('~/dev/kgems/k_templates/templates/tailwind') }
+  context 'live' do
+    let(:source_root_path) { File.expand_path('~/dev/kgems/k_templates/templates/tailwind') }
 
-#     before do
-#       instance.add_design_system(File.join(source_root_path, 'tui'))
-#       instance.add_design_system(File.join(source_root_path, 'noq'))
-#     end
+    before do
+      instance.add_design_system(File.join(source_root_path, 'tui'))
+      instance.add_design_system(File.join(source_root_path, 'noq'))
+    end
 
-#     it { instance.write(File.join(target_root_path, 'uikit.live.json')) }
-#   end
-# end
+    it { instance.write(File.join(target_root_path, 'uikit.live.json')) }
+  end
+end
 
-# RSpec.describe TailwindDsl::Etl::ComponentStructures::Generator do
-#   include_context :get_uikit
+RSpec.describe TailwindDsl::Etl::ComponentStructures::Generator do
+  include_context :get_uikit
 
-#   subject { instance }
+  subject { instance }
 
-#   let(:instance) { described_class.new(uikit, source_root_path, target_root_path) }
+  let(:instance) { described_class.new(uikit, source_root_path, target_root_path) }
 
-#   let(:source_root_path) { File.join(SPEC_FOLDER, 'samples/01-raw_components') }
-#   let(:target_root_path) { File.join(SPEC_FOLDER, 'samples/02-components') }
+  let(:source_root_path) { File.join(SPEC_FOLDER, 'samples/01-raw_components') }
+  let(:target_root_path) { File.join(SPEC_FOLDER, 'samples/02-components') }
 
-#   before do
-#     FileUtils.mkdir_p(target_root_path)
-#   end
+  before do
+    FileUtils.mkdir_p(target_root_path)
+  end
 
-#   it { instance.generate }
-# end
+  it { instance.generate }
+end
 
 # describe 'sample - gpt3 data extractor' do
 #   let(:extract_handler) { TailwindDsl::Etl::Extractors::DataExtractor }
